d437abae593a6267b4d6b9dd8b5591c6736fb0a1
AleixTP23219 <ateresp17@alumnes.ilerna.com>
2026-01-29 19:26:11 +0100

    Casi fet

--- src/main/java/HibernateSession.java
+++ src/main/java/HibernateSession.java
@@ -1,5 +1,3 @@
-package domain;
-
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,109 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import domain.HibernateSession;
+import domain.Car;
+import domain.Person;
+import domain.Vehicle;
+import domain.Motorcycle;
+import domain.Plane;
+import domain.Teacher;
+import domain.Student;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 
 public class Main {
 
+	private static List<Person> people = new ArrayList<>();
+	private static List<Vehicle> vehicles = new ArrayList<>();
 	public static void main(String[] args) {
-	
-		Session session = null;
-		Transaction transaction = null;
+
 		Scanner sc = new Scanner(System.in);
+		int opcio;
+
+		do {
+			System.out.println("\n===== MENÚ PRINCIPAL =====");
+			System.out.println("1) Fase 1: Crear dades de prova");
+			System.out.println("2) Fase 2: Treure vehicles de persones");
+			System.out.println("3) Fase 3: Actualitzar un vehicle");
+			System.out.println("0) Sortir");
+			System.out.print("Escull una opció: ");
+
+			opcio = sc.nextInt();
+			sc.nextLine();
+
+			switch (opcio) {
+				case 1:
+					fase1();
+					break;
+				case 2:
+					fase2();
+					break;
+				case 3:
+					fase3();
+					break;
+				case 0:
+					System.out.println("Fins aviat!");
+					break;
+				default:
+					System.out.println("Opció incorrecta.");
+			}
+
+		} while (opcio != 0);
+
+		sc.close();
+		HibernateSession.getSessionFactory().close();
+	}
 	
-		try {
-			session = HibernateSession.getSessionFactory().openSession();
-			transaction = session.beginTransaction();
-			
-		} catch (Exception ex) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			
-			ex.printStackTrace();
-		} finally {
-			if (session != null) {
-				session.close();
-			}
-			
-			HibernateSession.getSessionFactory().close();
-			sc.close();
-		}
+	private static void fase1() {
+        Session session=HibernateSession.getSessionFactory().openSession();
+        session.beginTransaction();
+        try {
+            Student juanjo= new Student("S001");
+            juanjo.setName("Juanjo");
+            juanjo.setSurname("Alveloa");
+            juanjo.setPhoneNumber(900000001);
+            session.persist(juanjo);
+
+            Teacher dulors= new Teacher("T001");
+            dulors.setName("Dulors");
+            dulors.setSurname("Martín");
+            dulors.setPhoneNumber(909090909);
+            session.persist(dulors);
+
+            Car nissan= new Car(4,5);
+            nissan.setBrand("Nissan");
+            nissan.setYear(2006);
+            nissan.setPrice(8900f);
+            juanjo.addVehicle(nissan);
+            session.persist(nissan);
+
+            Car twingo= new Car(5,5);
+            twingo.setBrand("Twingo");
+            twingo.setYear(2017);
+            twingo.setPrice(14000f);
+            dulors.addVehicle(twingo);
+            session.persist(twingo);
+
+            Motorcycle yamaha= new Motorcycle(false);
+            yamaha.setBrand("Yamaha");
+            yamaha.setYear(2015);
+            yamaha.setPrice(2100f);
+            dulors.ad
+
+
+
+        }catch (Exception e) {
+
+        };
+
+	}
+	
+	private static void fase2() {
+
+	}
+
+	private static void fase3() {
+
 	}
 }
